68b680ac790b80915246b78d5bb9c551ca253522
VisnevschiV <visnevskivlad@gmail.com>
2025-03-24 14:33:53 +0100

    extra-tutorial-out

--- Assets/_ProjectAssets/Scripts/Menu.cs
+++ Assets/_ProjectAssets/Scripts/Menu.cs
@@ -8,13 +8,9 @@ public class Menu : MonoBehaviour
 
     public UIDocument uiDocument;
     public GameObject game;
-    public TutorialData tutorialData;
-    public AudioSource click;
 
-    private VisualElement _tutorialWindow;
-    private VisualElement _practice;
-    private VisualElement _menu;
-    private int _tutorialStep;
+    private VisualElement _practiceWindow;
+    private VisualElement _menuWindow;
 
     void OnEnable()
     {
@@ -29,8 +25,6 @@ public class Menu : MonoBehaviour
         VisualElement rootVisualElement = uiDocument.rootVisualElement;
         rootVisualElement.Q<Button>("Play").clicked += PlayCompetitive;
         rootVisualElement.Q<Button>("practiceButton").clicked += Practice;
-        rootVisualElement.Q<Button>("HowTo").clicked += ActivateTutorial;
-        rootVisualElement.Q<Button>("Next").clicked += TutorialNext;
         rootVisualElement.Q<Button>("Play").clicked += PlayClick;
         rootVisualElement.Q<Button>("HowTo").clicked += PlayClick;
         rootVisualElement.Q<Button>("Next").clicked += PlayClick;
@@ -40,31 +34,30 @@ public class Menu : MonoBehaviour
                                                     (PlayerPrefs.HasKey("HighScore")
                                                         ? PlayerPrefs.GetInt("HighScore").ToString()
                                                         : "0");
-
-        _tutorialWindow = rootVisualElement.Q<VisualElement>("tutorialWindow");
-        _practice = rootVisualElement.Q<VisualElement>("practice");
-        _menu = rootVisualElement.Q<VisualElement>("buttonWrapper");
+        
+        _practiceWindow = rootVisualElement.Q<VisualElement>("practice");
+        _menuWindow = rootVisualElement.Q<VisualElement>("buttonWrapper");
     }
 
     private void PlayClick()
     {
-        click.Play();
+        AudioManager.Instance.PlayClick();
     }
 
     private void Practice()
     {
-        _practice.style.display = DisplayStyle.Flex;
-        _menu.style.display = DisplayStyle.None;
+        _practiceWindow.style.display = DisplayStyle.Flex;
+        _menuWindow.style.display = DisplayStyle.None;
         if (PlayerPrefs.HasKey("HighScore"))
         {
             int highScore = PlayerPrefs.GetInt("HighScore");
-            Button button = _practice.Q<Button>("easy");
+            Button button = _practiceWindow.Q<Button>("easy");
             button.clicked += () => PlayLevel(1);
             button.RemoveFromClassList("unavailableButton");
             button.Q<Label>().RemoveFromHierarchy();
             if (highScore > 4)
             {
-                button = _practice.Q<Button>("medium");
+                button = _practiceWindow.Q<Button>("medium");
                 button.clicked += () => PlayLevel(6);
                 button.RemoveFromClassList("unavailableButton");
                 button.Q<Label>().RemoveFromHierarchy();
@@ -72,7 +65,7 @@ public class Menu : MonoBehaviour
 
             if (highScore > 9)
             {
-                button = _practice.Q<Button>("hard");
+                button = _practiceWindow.Q<Button>("hard");
                 button.clicked += () => PlayLevel(15);
                 button.RemoveFromClassList("unavailableButton");
                 button.Q<Label>().RemoveFromHierarchy();
@@ -83,8 +76,8 @@ public class Menu : MonoBehaviour
     
     private void Home()
     {
-        _practice.style.display = DisplayStyle.None;
-        _menu.style.display = DisplayStyle.Flex;
+        _practiceWindow.style.display = DisplayStyle.None;
+        _menuWindow.style.display = DisplayStyle.Flex;
     }
 
     private void PlayCompetitive()
@@ -98,25 +91,6 @@ public class Menu : MonoBehaviour
         game.GetComponent<GameManager>().level = lvl;
         gameObject.SetActive(false);
         game.SetActive(true);
-        
-    }
-
-    private void ActivateTutorial(){
-        _tutorialWindow.style.display = DisplayStyle.Flex;
-        _tutorialStep=0;
-        _tutorialWindow.Q<VisualElement>("img").style.backgroundImage = tutorialData.tutorialElements[_tutorialStep].sprite.texture;
-        _tutorialWindow.Q<Label>().text = tutorialData.tutorialElements[_tutorialStep].text;
-        _tutorialStep++;
-    }
-
-    private void TutorialNext(){
-        if(_tutorialStep > tutorialData.tutorialElements.Count-1){
-             _tutorialWindow.style.display = DisplayStyle.None;
-             return;
-        }
-        _tutorialWindow.Q<VisualElement>("img").style.backgroundImage = tutorialData.tutorialElements[_tutorialStep].sprite.texture;
-        _tutorialWindow.Q<Label>().text = tutorialData.tutorialElements[_tutorialStep].text;
-        _tutorialStep++;
         
     }
 }
